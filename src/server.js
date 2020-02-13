8ba0f7211b7238226c51def293a4f4f7a8a0dcfd
Matt Coy <fluffytoycoy@gmail.com>
2020-02-13 17:27:26 -0600

    added comments and readme.md tracker for path basic setup of server

--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const morgan = require('morgan');
 
+// Create basic server
 const app = express();
 
+// Set host port to env file or 6161 in DEV
 const port = process.env.PORT || 6161;
 
+// listen on port
 app.listen(port, () => {
   console.log(`App listening on localhost:${port}`);
 });
