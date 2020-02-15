2481e6881083df1ff4ffb2901ed32b1c53c9493f
Matt Coy <fluffytoycoy@gmail.com>
2020-02-15 02:02:19 -0600

    lint fixes and multer added for upcoming image uploads

--- src/api/locations.js
+++ src/api/locations.js
@@ -5,19 +5,25 @@ const { push, returnCopy } = require('./utils/mongooseModifiers');
 
 const router = Router();
 
+// GET - All locations
 router.get('/', async (req, res) => {
   const locations = await Location.find();
   res.json(locations);
 });
 
+// GET - Location by id
 router.get('/location-id', async (req, res) => {
-  const locations = await Location.findById('5e47868af3289639405efd3a');
+  const locationId = req.body.id;
+  // GET Location by id
+  const locations = await Location.findById(locationId);
   res.json(locations);
 });
 
 router.put('/add-image', async (req, res, next) => {
   const locationId = req.body.id;
+  // BUILD image for upload
   const image = new Image({ originalSrc: 'www.newImage.com' });
+  // Update Location with image
   Location.findByIdAndUpdate(locationId, push({ images: image }), returnCopy)
     .then((newLocation) => {
       res.json(newLocation.images);
@@ -26,19 +32,16 @@ router.put('/add-image', async (req, res, next) => {
     });
 });
 
-// router.post('/', (req, res, next) => {
-//   const location = new Location(req.body);
-//   location.save().then((entry) => {
-//     res.json(entry);
-//   }).catch((error) => {
-//     if (error.name === 'ValidationError') {
-//       res.status(422);
-//     }
-//     next(error)
-//   });
-// });
-// router.post('/', (req, res, next) => {
-//
-// });
+router.post('/', (req, res, next) => {
+  const location = new Location(req.body);
+  location.save().then((entry) => {
+    res.json(entry);
+  }).catch((error) => {
+    if (error.name === 'ValidationError') {
+      res.status(422);
+    }
+    next(error);
+  });
+});
 
 module.exports = router;
