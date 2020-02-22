1e9080bd485745affad93320c0734d1dc76f683f
Matt Coy <fluffytoycoy@gmail.com>
2020-02-22 14:20:21 -0600

    user now popoulated with locations and followed users

--- src/api/locations.js
+++ src/api/locations.js
@@ -9,15 +9,21 @@ const { auth } = require('../middlewares/middlewares');
 router.get('/', auth, async (req, res) => {
   const userId = req.body.user_id;
   // Get logged in user Info
-  const user = await User.findOne({ _id: userId }).populate('following');
+  const user = await User.findOne({ _id: userId }).populate({
+    path: 'following',
+    populate: {
+      path: 'locations',
+      model: 'Location'
+    }
+  }).populate('locations');
   // Get all locations for user and users friends
   // const locations = await Location.find({ user_id: { $in: [userId, ...user.following] } });
   // // Get all friends of user
   // const following = await User.find({ _id: { $in: [...user.following] } });
   // Add locations and users you are following to User object
-  console.log(user, 'user')
-  console.log(user.locations, 'locations')
-  console.log(user.following, 'following')
+  console.log(user, 'user');
+  console.log(user.locations, 'locations');
+  console.log(user.following, 'following');
   res.json(user);
 });
 
@@ -43,9 +49,12 @@ router.put('/add-image', auth, async (req, res, next) => {
 });
 
 router.post('/', auth, async (req, res, next) => {
+  const userId = req.body.user_id;
   const location = new Location(req.body);
   await location.save().then((entry) => {
-    res.json(entry);
+    User.findByIdAndUpdate(userId, { $addToSet: { locations: location } })
+      .then(() => res.json(entry))
+      .catch((err) => next(err));
   }).catch((error) => {
     if (error.name === 'ValidationError') {
       res.status(422);
