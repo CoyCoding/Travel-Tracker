86066e4a36e5d1588cb89da7cfa47c4630b6ae43
Matt Coy <fluffytoycoy@gmail.com>
2020-02-23 04:35:49 -0600

    clean up of location posting by user

--- src/api/locations.js
+++ src/api/locations.js
@@ -13,41 +13,13 @@ router.get('/', auth, async (req, res) => {
     path: 'following',
     populate: {
       path: 'locations',
-      model: 'Location'
-    }
+      model: 'Location',
+    },
   }).populate('locations');
-  // Get all locations for user and users friends
-  // const locations = await Location.find({ user_id: { $in: [userId, ...user.following] } });
-  // // Get all friends of user
-  // const following = await User.find({ _id: { $in: [...user.following] } });
-  // Add locations and users you are following to User object
-  console.log(user, 'user');
-  console.log(user.locations, 'locations');
-  console.log(user.following, 'following');
   res.json(user);
 });
 
-// GET - Location by id
-router.get('/byId', auth, async (req, res) => {
-  const locationId = req.body.id;
-  // GET Location by id
-  const locations = await Location.findById(locationId);
-  res.json(locations);
-});
-
-router.put('/add-image', auth, async (req, res, next) => {
-  const locationId = req.body.id;
-  // BUILD image for upload
-  const image = new Image({ originalSrc: 'www.newImage.com' });
-  // Update Location with image
-  Location.findByIdAndUpdate(locationId, push({ images: image }), returnCopy)
-    .then((newLocation) => {
-      res.json(newLocation.images);
-    }).catch((error) => {
-      next(error);
-    });
-});
-
+// POST - Add location to posting user
 router.post('/', auth, async (req, res, next) => {
   const userId = req.body.user_id;
   const location = new Location(req.body);
@@ -61,6 +33,28 @@ router.post('/', auth, async (req, res, next) => {
     }
     next(error);
   });
+});
+
+// GET - Location by id
+router.get('/byId', auth, async (req, res) => {
+  const locationId = req.body.id;
+  // GET Location by id
+  const locations = await Location.findById(locationId);
+  res.json(locations);
+});
+
+// POST - Create an image on the server
+router.put('/add-image', auth, async (req, res, next) => {
+  const locationId = req.body.id;
+  // BUILD image for upload
+  const image = new Image({ originalSrc: 'www.newImage.com' });
+  // Update Location with image
+  Location.findByIdAndUpdate(locationId, push({ images: image }), returnCopy)
+    .then((newLocation) => {
+      res.json(newLocation.images);
+    }).catch((error) => {
+      next(error);
+    });
 });
 
 module.exports = router;
