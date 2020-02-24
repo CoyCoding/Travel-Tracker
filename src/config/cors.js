c324de548fce15e21d77c09865ab09da74e04c56
Matt Coy <fluffytoycoy@gmail.com>
2020-02-24 11:12:34 -0600

    switched to localdb for extensive testing

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,4 +1,5 @@
 module.exports = {
   // Whitelisted domains for accessing the api
-  origin: [process.env.CORS_ORIGIN_CLIENT, 'http://localhost:3000/', 'localhost:3000', 'http://localhost:3000'],
+  origin: [process.env.CORS_ORIGIN_CLIENT, 'http://localhost:3000/', 'localhost:3000', 'http://localhost:3000', 'mongodb://localhost:27102', '127.0.0.1:27017'],
+  exposedHeaders: 'access-token',
 };
